240be4a787a5f8349b7ceb599e48ea58aa79027f
AFz <afz.badie@gmail.com>
2024-07-07 23:44:33 +0330

    add pulseaudio control

--- setup.sh
+++ setup.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-sudo pacman -Sy --noconfirm git vim github-cli curl wget zsh ttf-hack papirus-icon-theme awesome-terminal-fonts rofi picom xfce4-terminal lxappearance nitrogen pcmanfm ranger ttf-jetbrains-mono-nerd python-pip python-virtualenv python-psutil htop neofetch thunar zip unzip scrot tumbler ffmpegthumbnailer webp-pixbuf-loader poppler-glib gvfs-mtp qt5-tools
+sudo pacman -Sy --noconfirm git vim github-cli curl wget zsh ttf-hack papirus-icon-theme awesome-terminal-fonts rofi picom xfce4-terminal lxappearance nitrogen pcmanfm ranger ttf-jetbrains-mono-nerd python-pip python-virtualenv python-psutil htop neofetch thunar zip unzip scrot tumbler ffmpegthumbnailer webp-pixbuf-loader poppler-glib gvfs-mtp qt5-tools pavucontrol
+
+cp -r .config ~/.config
+cp -r .local  ~/.local
+cp -r .themes ~/.themes
+cp -r .icons  ~/.icons
+cp .vimrc ~/.vimrc
+cp .zshrc ~/.zshrc
+
 echo "Completed"
